981e15e76ded54080d339ab2bbe2edb3c39ac745
Suko Aria <Ariadnesuko@hotmail.com>
2026-06-06 18:44:04 +0800

    feat(analytics): rewrite service with real DB queries + mock data, expand controller to 20+ endpoints

--- packages/backend/src/modules/analytics/analytics.module.ts
+++ packages/backend/src/modules/analytics/analytics.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
+import { GenerationTask } from '../generation/entities/generation-task.entity';
+import { Material } from '../materials/entities/material.entity';
+import { Script } from '../scripts/entities/script.entity';
+import { Template } from '../templates/entities/template.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([GenerationTask, Material, Script, Template])],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
   exports: [AnalyticsService],
