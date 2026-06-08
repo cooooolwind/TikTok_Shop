cde8a34358ea82ae14b5d383cccb374642adbf54
Suko Aria <Ariadnesuko@hotmail.com>
2026-06-08 15:42:52 +0800

    feat(scripts): support imitation mode via reference video injection

--- packages/backend/src/modules/scripts/scripts.module.ts
+++ packages/backend/src/modules/scripts/scripts.module.ts
@@ -6,11 +6,16 @@ import { Script } from './entities/script.entity';
 import { Scene } from './entities/scene.entity';
 import { Material } from '../materials/entities/material.entity';
 import { TemplatesModule } from '../templates/templates.module';
+import { ReferencesModule } from '../references/references.module';
 import { GenerationTask } from '../generation/entities/generation-task.entity';
 import { Video } from '../generation/entities/video.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Script, Scene, Material, GenerationTask, Video]), TemplatesModule],
+  imports: [
+    TypeOrmModule.forFeature([Script, Scene, Material, GenerationTask, Video]),
+    TemplatesModule,
+    ReferencesModule,
+  ],
   controllers: [ScriptsController],
   providers: [ScriptsService],
   exports: [ScriptsService],
